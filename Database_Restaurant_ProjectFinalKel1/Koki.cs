e9402bf6dff85ef80130b99b259be9bcfcbbf158
mohfirdaus22 <thepashter6@gmail.com>
2023-07-11 14:13:18 +0700

    Add fungtion connection dan refreshform diform koki

--- Database_Restaurant_ProjectFinalKel1/Koki.cs
+++ Database_Restaurant_ProjectFinalKel1/Koki.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,34 @@ namespace Database_Restaurant_ProjectFinalKel1
 {
     public partial class Koki : Form
     {
+
+        //connection ke sql
+        private string stringConnection = "data source=Jorengezzz\\THEPASHTER;" + "database=Restaurant_K1;User ID=sa;password=Salahtompo22";
+        private SqlConnection koneksi;
+
+
+        //membuat sndiri refresh form
+        private void refreshform()
+        {
+            txtidkoki.Text = "";
+            txtnama.Text = "";
+            txtnotelp.Text = "";
+            txtalamat.Text = "";
+
+            btnclear.Enabled = false;
+            btnsave.Enabled = false;
+            btnopen.Enabled = false;
+
+        }
+
         public Koki()
         {
             InitializeComponent();
+        }
+
+        private void Koki_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
