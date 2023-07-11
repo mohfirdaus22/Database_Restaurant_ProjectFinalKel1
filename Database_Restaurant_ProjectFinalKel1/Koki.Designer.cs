34b7d084c665a94d687c40e8218e87da46d85e4f
mohfirdaus22 <thepashter6@gmail.com>
2023-07-11 14:09:28 +0700

    Add datagridview,textbox,dan button di form koki

--- Database_Restaurant_ProjectFinalKel1/Koki.Designer.cs
+++ Database_Restaurant_ProjectFinalKel1/Koki.Designer.cs
@@ -28,12 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtidkoki = new System.Windows.Forms.TextBox();
+            this.txtnama = new System.Windows.Forms.TextBox();
+            this.txtnotelp = new System.Windows.Forms.TextBox();
+            this.txtalamat = new System.Windows.Forms.TextBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnback = new System.Windows.Forms.Button();
+            this.btnopen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(5, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(789, 166);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID Koki";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(176, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nama Koki";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 294);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "No Telp";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(176, 320);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Alamat";
+            // 
+            // txtidkoki
+            // 
+            this.txtidkoki.Location = new System.Drawing.Point(299, 237);
+            this.txtidkoki.Name = "txtidkoki";
+            this.txtidkoki.Size = new System.Drawing.Size(266, 22);
+            this.txtidkoki.TabIndex = 5;
+            // 
+            // txtnama
+            // 
+            this.txtnama.Location = new System.Drawing.Point(299, 262);
+            this.txtnama.Name = "txtnama";
+            this.txtnama.Size = new System.Drawing.Size(266, 22);
+            this.txtnama.TabIndex = 6;
+            // 
+            // txtnotelp
+            // 
+            this.txtnotelp.Location = new System.Drawing.Point(299, 288);
+            this.txtnotelp.Name = "txtnotelp";
+            this.txtnotelp.Size = new System.Drawing.Size(266, 22);
+            this.txtnotelp.TabIndex = 7;
+            // 
+            // txtalamat
+            // 
+            this.txtalamat.Location = new System.Drawing.Point(299, 314);
+            this.txtalamat.Name = "txtalamat";
+            this.txtalamat.Size = new System.Drawing.Size(266, 22);
+            this.txtalamat.TabIndex = 8;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(233, 385);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(78, 33);
+            this.btnadd.TabIndex = 9;
+            this.btnadd.Text = "Add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(384, 385);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(78, 33);
+            this.btnsave.TabIndex = 10;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(538, 385);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(78, 33);
+            this.btnclear.TabIndex = 11;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            // 
+            // btnback
+            // 
+            this.btnback.Location = new System.Drawing.Point(12, 16);
+            this.btnback.Name = "btnback";
+            this.btnback.Size = new System.Drawing.Size(78, 33);
+            this.btnback.TabIndex = 12;
+            this.btnback.Text = "Back";
+            this.btnback.UseVisualStyleBackColor = true;
+            // 
+            // btnopen
+            // 
+            this.btnopen.Location = new System.Drawing.Point(710, 12);
+            this.btnopen.Name = "btnopen";
+            this.btnopen.Size = new System.Drawing.Size(78, 33);
+            this.btnopen.TabIndex = 13;
+            this.btnopen.Text = "Open";
+            this.btnopen.UseVisualStyleBackColor = true;
+            // 
+            // Koki
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnopen);
+            this.Controls.Add(this.btnback);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.txtalamat);
+            this.Controls.Add(this.txtnotelp);
+            this.Controls.Add(this.txtnama);
+            this.Controls.Add(this.txtidkoki);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Koki";
             this.Text = "Koki";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtidkoki;
+        private System.Windows.Forms.TextBox txtnama;
+        private System.Windows.Forms.TextBox txtnotelp;
+        private System.Windows.Forms.TextBox txtalamat;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnback;
+        private System.Windows.Forms.Button btnopen;
     }
 }
